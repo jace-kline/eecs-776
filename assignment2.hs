3417d1d35acaaf9e55a58914f5c40503621542ab
Jace Kline <jace_kline@ku.edu>
2020-02-12 16:48:40 -0600

    Assignment 2

---
+++ assignment2.hs
@@ -0,0 +1,35 @@
+
+sum' :: [Int] -> Int
+sum' []     = 0
+sum' (x:xs) = x + sum xs
+
+sum'' :: [Int] -> Int
+sum'' xs = if null xs then 0 else head xs + sum (tail xs)
+
+take' :: Int -> [a] -> [a]
+take' 0 _      = []
+take' _ []     = []
+take' n (x:xs) = x : take (n-1) xs
+
+take'' :: Int -> [a] -> [a]
+take'' n xs = if (null xs || n == 0) then [] else head xs : take (n-1) (tail xs)
+
+drop' :: Int -> [a] -> [a]
+drop' 0 xs     = xs
+drop' _ []     = []
+drop' n (x:xs) = drop' (n-1) xs
+
+drop'' :: Int -> [a] -> [a]
+drop'' n xs = if null xs then []
+              else
+                if n == 0 then xs
+                else drop'' (n-1) (tail xs)
+
+last' :: [a] -> a
+last' (x:[]) = x
+last' (x:xs) = last' xs
+
+last'' :: [a] -> a
+last'' xs = case xs of
+            (x:[]) -> x
+            (x:xs) -> last'' xs
